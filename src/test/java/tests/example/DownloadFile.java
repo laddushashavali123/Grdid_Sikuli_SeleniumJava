e33dab61a23ec73204e4013fe93cd38d579a2910
kadung <kieuanhdung90@gmail.com>
2018-02-07 17:37:09 +0700

    updated

--- src/test/java/tests/example/DownloadFile.java
+++ src/test/java/tests/example/DownloadFile.java
@@ -1,4 +1,17 @@
+
+// https://sqa.stackexchange.com/questions/2197/how-to-download-a-file-using-seleniums-webdriver
+
 package tests.example;
 
-public class DownloadFile {
+import base.DriverBase;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class DownloadFile extends DriverBase{
+    @Test
+    public void test1(){
+        WebDriver driver = getDriver();
+
+
+    }
 }
