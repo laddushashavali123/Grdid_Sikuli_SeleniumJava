b1d15d7c9014ee991d117d7c0c88191e6827047e
kadung <kieuanhdung90@gmail.com>
2018-03-26 00:03:21 +0700

    updated

--- src/test/java/tests/example/JDBC_MySQL.java
+++ src/test/java/tests/example/JDBC_MySQL.java
@@ -1,7 +1,15 @@
 package tests.example;
 
+import utils.database.MySQLConnUtils;
+
 import java.sql.*;
 
 
 public class JDBC_MySQL {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        System.out.println("Get connection ... ");
+        Connection conn = MySQLConnUtils.getMySQLConnection();
+        System.out.println("Connected to " + conn);
+
+    }
 }
--- src/test/java/utils/database/MySQLConnUtils.java
+++ src/test/java/utils/database/MySQLConnUtils.java
@@ -5,22 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQLConnUtils {
-    String hostName = "localhost";
-    String dbName   = "simplehr";
-    String userName = "root";
-    String password = "1234";
+    // Kết nối vào MySQL.
+    public static Connection getMySQLConnection() throws SQLException,
+            ClassNotFoundException {
+        String hostName = "localhost";
+        String dbName   = "myblog";
+        String userName = "root";
+        String password = "root";
+
+        return getMySQLConnection(hostName, dbName, userName, password);
+    }
+
 
     public static Connection getMySQLConnection(String hostName, String dbName, String userName, String password) throws SQLException,ClassNotFoundException {
-        /** Khai báo class Driver cho DB MySQL: Việc này cần thiết với Java 5, Java6 tự động tìm kiếm Driver thích hợp.
-         *  Nếu bạn dùng Java6, thì ko cần dòng này cũng được.
-         */
-        // Class.forName("com.mysql.jdbc.Driver");
 
-        /** Cấu trúc URL Connection dành cho Oracle
-         *  Ví dụ: jdbc:mysql://localhost:3306/simplehr
-         */
 
-        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName + "?useSSL=false";
 
         Connection con = DriverManager.getConnection(connectionURL, userName, password);
         return con;
